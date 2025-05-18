9c209bd837dd19ea2244b5275cb2091e6b103284
taskeen-mustafa <taskeenmustafanizamani.bscsf22@iba-suk.edu.pk>
2025-05-18 18:34:44 +0500

    Initiallized and configured env file

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express()
 
 app.get('/',(req,res)=>res.send('App is running'))
 
-const port = 5500
+const port = process.env.PORT || 5500
 app.listen(port,()=>console.log("Server is running on port# ",port))
